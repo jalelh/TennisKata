f2e26367f7e723e95882e64fbc2b803f2a179c37
jalelh <h.jaloul@yahoo.fr>
2017-08-12 08:08:48 +0200

    Initial Commit''

--- AdneomKata2/src/main/java/kata/tennis/Tennis.java
+++ AdneomKata2/src/main/java/kata/tennis/Tennis.java
@@ -1,6 +1,18 @@
 package kata.tennis;
 
 public class Tennis {
+	
+	private String nomPremierJoueur;
+	private String nomDeuxiemeJoueur;
+	
+	public Tennis(String nomPremierJoueur, String nomDeuxiemeJoueur) {
+		this.nomPremierJoueur = nomPremierJoueur;
+		this.nomDeuxiemeJoueur = nomDeuxiemeJoueur;
+	}
+	
+	public String retourneScore() {
+		return "Love all";
+	}
 
 
 }
--- AdneomKata2/src/test/java/kata/tennis/TennisTest.java
+++ AdneomKata2/src/test/java/kata/tennis/TennisTest.java
@@ -7,8 +7,11 @@ import org.junit.Test;
 public class TennisTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void retourneLoveAllSiNouveauMatch()
+	{	
+		Tennis match = new Tennis("Rafael Nadal", "Roger Federer");
+		assertEquals(match.retourneScore(), "Love all");
+		
 	}
 
 }
